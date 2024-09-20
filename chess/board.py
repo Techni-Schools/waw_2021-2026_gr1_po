19880e534c8ba86b1373c2cf49f1f31544f998e2
adrianlukasik <adrian.lukasik@technischools.com>
2024-09-20 13:32:35 +0200

    Fixed board

--- chess/board.py
+++ chess/board.py
@@ -8,20 +8,19 @@ from field import Field
 
 class Board:
     def __init__(self):
-        first_row = [Field(Rook()),
-                       Field(Knight()),
-                       Field(Bishop()),
-                       Field(Queen()),
-                       Field(King()),
-                       Field(Bishop()),
-                       Field(Knight()),
-                       Field(Rook())]
-        self.board = [
-                    first_row,
-                    [Field(Pawn()) for _ in range(8)],
-                    *[[Field() for _ in range(8)] for _ in range(6)]
-                    ]
+        first_row = [Rook(), Knight(), Bishop(), Queen(), King(), Bishop(), Knight(), Rook()]
+        self.board = [[Field(piece),Field(Pawn()),*[Field() for _ in range(6)]] for piece in first_row]
 
+        # self.board = [
+        #     [Field(Rook()),Field(Pawn()),*[Field() for _ in range(6)]],
+        #     [Field(Knight()),Field(Pawn()),*[Field() for _ in range(6)]],
+        #     [Field(Bishop()),Field(Pawn()),*[Field() for _ in range(6)]],
+        #     [Field(Queen()),Field(Pawn()),*[Field() for _ in range(6)]],
+        #     [Field(King()),Field(Pawn()),*[Field() for _ in range(6)]],
+        #     [Field(Bishop()),Field(Pawn()),*[Field() for _ in range(6)]],
+        #     [Field(Knight()),Field(Pawn()),*[Field() for _ in range(6)]],
+        #     [Field(Rook()),Field(Pawn()),*[Field() for _ in range(6)]],
+        # ]
 
     def print_board(self):
         for i in range(7, -1, -1):
@@ -33,20 +32,30 @@ class Board:
         for i in range(8):
                 print(chr(i + 65), end="    ")
 
-    def check_field(self, x1: int, y1: int, x2: int, y2: int) -> bool:
-        if self.board[x1][y1].piece is not None:
+    def is_valid_move(self, x1: int, y1: int, x2: int, y2: int) -> bool:
+        piece = self.board[x1][y1].piece
+        if not piece:
             return False
         if self.board[x2][y2].piece:
             return False
-        piece = self.board[x1][y1].piece
-        list_of_moves = piece.get_moves(y1, x1)
+
+        list_of_moves = piece.get_moves(x1, y1)
         if (x2, y2)  not in list_of_moves:
             return False
+        moves_between = piece.get_moves_between(x1, y1, x2, y2)
+        for i,j in moves_between:
+            if self.board[i][j].piece:
+                return False
+        return True
+
 
 
 
 board = Board()
 board.print_board()
+print(board.is_valid_move(1,1,1,2))
+print(board.is_valid_move(0,0,0,2))
+print(board.is_valid_move(0,0,1,6))
 
 
 
