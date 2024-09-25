a1a4c8eb6622b50cdfd8595140a9985f1b193658
al <adrian.lukasik@technischools.com>
2024-09-25 13:03:17 +0200

    Added make move

--- chess/board.py
+++ chess/board.py
@@ -6,10 +6,11 @@ from king import King
 from bishop import Bishop
 from field import Field
 
+
 class Board:
     def __init__(self):
         first_row = [Rook(), Knight(), Bishop(), Queen(), King(), Bishop(), Knight(), Rook()]
-        self.board = [[Field(piece),Field(Pawn()),*[Field() for _ in range(6)]] for piece in first_row]
+        self.board = [[Field(piece), Field(Pawn()), *[Field() for _ in range(6)]] for piece in first_row]
 
         # self.board = [
         #     [Field(Rook()),Field(Pawn()),*[Field() for _ in range(6)]],
@@ -25,12 +26,13 @@ class Board:
     def print_board(self):
         for i in range(7, -1, -1):
             print(i + 1, end=' ')
-            for j in range(7, -1, -1):
-                print(self.board[i][j], end=" ")
+            for j in range(8):
+                print(self.board[j][i], end=' ')
             print()
-        print("   " , end='')
+        print("   ", end='')
         for i in range(8):
-                print(chr(i + 65), end="    ")
+            print(chr(i + 65), end="    ")
+        print()
 
     def is_valid_move(self, x1: int, y1: int, x2: int, y2: int) -> bool:
         piece = self.board[x1][y1].piece
@@ -40,24 +42,31 @@ class Board:
             return False
 
         list_of_moves = piece.get_moves(x1, y1)
-        if (x2, y2)  not in list_of_moves:
+        if (x2, y2) not in list_of_moves:
             return False
         moves_between = piece.get_moves_between(x1, y1, x2, y2)
-        for i,j in moves_between:
+        for i, j in moves_between:
             if self.board[i][j].piece:
                 return False
         return True
+
+    def make_move(self, x1: int, y1: int,x2: int, y2: int ):
+        if self.is_valid_move(x1, y1, x2, y2):
+            self.board[x2][y2].piece = self.board[x1][y1].piece
+            self.board[x1][y1].piece = None
+        else:
+            print("nie mozna wykonac ruchu")
 
 
 
 
 board = Board()
 board.print_board()
-print(board.is_valid_move(1,1,1,2))
-print(board.is_valid_move(0,0,0,2))
-print(board.is_valid_move(0,0,1,6))
+print(board.is_valid_move(1, 1, 1, 2))
+print(board.is_valid_move(0, 0, 0, 2))
+print(board.is_valid_move(0, 0, 1, 6))
 
-
-
-
-
+board.make_move(1,1,1,2)
+# board.make_move(0,0,1,2)
+# board.make_move(0,1,1,4)
+board.print_board()
