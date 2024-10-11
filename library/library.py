ac271e0786ea784e55ab8be7390a45e882064858
al <adrian.lukasik@technischools.com>
2024-10-11 10:10:40 +0200

    Added display books by genre

--- library/library.py
+++ library/library.py
@@ -1,3 +1,4 @@
+from functools import *
 from user import User
 from book import Book
 
@@ -9,20 +10,26 @@ class BookAlreadyExists(Exception):
 class UserAlreadyExists(Exception):
     pass
 
+
 class BookAlreadyRented(Exception):
     pass
+
 
 class UserDoesNotExists(Exception):
     pass
 
+
 class BookTitleNotExists(Exception):
     pass
+
 
 class BookDoesNotExists(Exception):
     pass
 
+
 class BookDoesNotRented(Exception):
     pass
+
 
 class Library:
     def __init__(self):
@@ -78,20 +85,50 @@ class Library:
 
         raise BookDoesNotRented()
 
-    def display_books_title_by_genre(self, genre:str ):
+    def display_books_title_by_genre(self, genre: str):
+        book_count = 0
         for title, books in self.books.items():
             if genre in map(lambda book: book.genre, books):
+                book_count += 1
                 print(title)
+
+        if book_count == 0:
+            print(f'Brak książek o gatunku {genre}')
+
+    def print_genre_by_author(self, author:str):
+        books = reduce(list.__add__, self.books.values())
+        books = filter(lambda book: book.author == author, books)
+        books = map(lambda book: book.genre, books)
+        books = set(books)
+        print(books)
+
+    def add_books(self, *args: Book):
+        for book in args:
+            self.add_book(book)
+
+    def add_users(self, *args: User):
+        for user in args:
+            self.add_user(user)
+
 
 
 library = Library()
 book_1 = Book("Zbrodnia Ikara", "Fiodor Dostojewski", "dramat", 2018)
-library.add_book(book_1)
-print(library.books)
+book_2 = Book("Lalka", "Bolesław Prus", "sci-fi", 2018)
+book_3 = Book("Adam Mickiewicz", "Pan Tadeusz", "fantastyka", 2018)
+book_4 = Book("Hobbit", "JRR Tolkien", "fantastyka", 2018)
+book_5 = Book("Mistrz Małgorzata", "Michal Buhałkow", "fantasy", 2018)
+book_6 = Book("Adam Czynsz", "Pan Tadeusz", "fantastyka", 2018)
+book_7 = Book("Adam Mickiewicz", "Pan Tadeusz", "fantasy", 2018)
+book_8 = Book("Adam Mickiewicz", "Pan Tadeusz", "komedia", 2018)
+library.add_books(book_1, book_2, book_3, book_4, book_5, book_6, book_7, book_8)
+# print(library.books)
 
-user_1 = User("Bartosz", "Prejs", "6969696969", "dupa@gmail.com")
-library.add_user(user_1)
-print(library.users)
+user_1 = User("Bartosz", "Rejs", "6969696969", "dupa@gmail.com")
+user_2 = User("Adam", "Czyz", "6969696969", "dupa@gmail.com")
+user_3 = User("Maks", "Wiewiora", "6969696969", "dupa@gmail.com")
+library.add_users(user_1, user_2, user_3)
+# print(library.users)
 try:
     library.rent_book(user_1.id, book_1)
 except UserDoesNotExists:
@@ -107,3 +144,6 @@ try:
     library.return_book(book_1.id)
 except BookDoesNotRented:
     print("Book didn't rent")
+
+# library.display_books_title_by_genre("fantastyka1")
+library.print_genre_by_author("Pan Tadeusz")
